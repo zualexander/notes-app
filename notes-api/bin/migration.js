fa763b4ca8fe9a7e75414ceac69baa1afdfc2f88
Alexander Zulechner <alexanderzulechner@gmail.com>
2017-03-04 17:39:55 +0100

    chore(api): adds configuration for postgres_db

---
+++ notes-api/bin/migration.js
@@ -0,0 +1,14 @@
+MySQL.prototype.autoupdate = function (models, cb) {
+  //todo: create coorect migration files and script
+  // CREATE TABLE public.note
+  // (
+  //   id VARCHAR NOT NULL,
+  //   createdAt DATE,
+  //   title VARCHAR,
+  //   content VARCHAR
+  // );
+};
+
+MySQL.prototype.automigrate = function (models, cb) {
+  // ...
+};
